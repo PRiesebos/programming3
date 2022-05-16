b0c7a474521201e381b951c08bf442519d2f89ce
Peter Riesebos <priesebos@live.com>
2022-05-16 13:09:33 +0200

    finished fetch, write and main function

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,26 +1,38 @@
+from Bio import Entrez
 import argparse as ap
+import multiprocessing as mp
+from time import sleep
 
-handle = Entrez.efetch(
-    db="pmc",
-    id=pubmed_id,
-    rettype="XML",
-    retmode="text",
-    api_key="1b128aaba77c37664c213d753017ca520108",
-)
-with open(f"output/{pubmed_id}.xml", "wb") as file:
-    file.write(handle.read())
 
-Entrez.email = "pcriesebos@gmail.com"
-results = Entrez.read(
-    Entrez.elink(
-        dbfrom="pubmed",
-        db="pmc",
-        LinkName="pubmed_pmc_refs",
-        id=pubmed_id,
-        api_key="INSERT YOUR PUBMED API KEY HERE!",
+def get_references(pubmed_id):
+    Entrez.email = "pcriesebos@gmail.com"
+    references = []
+
+    results = Entrez.read(
+        Entrez.elink(
+            dbfrom="pubmed",
+            db="pmc",
+            LinkName="pubmed_pmc_refs",
+            id=pubmed_id,
+            api_key="1b128aaba77c37664c213d753017ca520108",
+        )
     )
-)
-references = [f'{link["Id"]}' for link in results[0]["LinkSetDb"][0]["Link"]]
+
+    references = [f'{link["Id"]}' for link in results[0]["LinkSetDb"][0]["Link"]]
+
+    return references
+
+
+def write_to_xml(pubmed_id):
+    handle = Entrez.efetch(
+        db="pmc",
+        id=pubmed_id,
+        retmode="xml",
+        api_key="1b128aaba77c37664c213d753017ca520108",
+    )
+    with open(f"output/{pubmed_id}.xml", "wb") as file:
+        file.write(handle.read())
+    sleep(15)
 
 
 if __name__ == "__main__":
@@ -44,3 +56,9 @@ if __name__ == "__main__":
     )
     args = argparser.parse_args()
     print("Getting: ", args.pubmed_id)
+
+    ids = get_references(args.pubmed_id)[:10]
+
+    cpus = mp.cpu_count()
+    with mp.Pool(cpus) as pool:
+        results = pool.map(write_to_xml, ids)
