9d73ddb7f5eaeb2c6182f88b4514ce35abd91dbc
Peter Riesebos <priesebos@live.com>
2022-05-16 14:11:51 +0200

    cleanup

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,7 +1,7 @@
 from Bio import Entrez
+from time import sleep
 import argparse as ap
 import multiprocessing as mp
-from time import sleep
 
 
 def get_references(pubmed_id):
@@ -27,7 +27,8 @@ def write_to_xml(pubmed_id):
     handle = Entrez.efetch(
         db="pmc",
         id=pubmed_id,
-        retmode="xml",
+        rettype="XML",
+        retmode="text",
         api_key="1b128aaba77c37664c213d753017ca520108",
     )
     try:
@@ -58,13 +59,13 @@ if __name__ == "__main__":
         nargs=1,
         help="Pubmed ID of the article to harvest for references to download.",
     )
+
     args = argparser.parse_args()
-
+    cpus = mp.cpu_count()
     count = args.n or 10
-    print(f"Getting: {count} articles for {args.pubmed_id}")
-
     ids = get_references(args.pubmed_id)[:count]
 
-    cpus = mp.cpu_count()
+    print(f"Getting: {count} articles for {args.pubmed_id}")
+
     with mp.Pool(cpus) as pool:
         results = pool.map(write_to_xml, ids)
