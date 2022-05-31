c9e15089e5f550820b73041c241306c77c8f5136
Peter Riesebos <priesebos@live.com>
2022-05-31 11:36:16 +0200

    finished scripts

--- Assignment3/assignment3.py
+++ Assignment3/assignment3.py
@@ -1,11 +1,17 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('timings.txt', sep="", header=None)
-y = data.blabla
-x = np.linspace(1, 17)
+data = pd.read_csv(
+    "/homes/pcriesebos/Documents/programming3/Assignment3/output/timings.txt",
+    sep="\t",
+    lineterminator="\n",
+    header=None,
+)
+x = data.iloc[:, 0]
+y = data.iloc[:, 1]
+plt.scatter(x, y)
 plt.plot(x, y)
-
-# load timings.txt
-# export timings.png
+plt.title("Blastp runtime with variating thread count")
+plt.xlabel("Number of threads")
+plt.ylabel("Time taken (s)")
+plt.savefig("output/timings.png")
